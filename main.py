9ac284122c59a2476f4d44a39b158ba68b999c4e
Augusto Borges <139978458+GUT-R@users.noreply.github.com>
2026-09-15 12:10:13 +0000

    idk

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-class Neurônio:
-    def __init__(self, conexoes: list[int]):
+from typing import Callable, Any, Optional
+red = '\033[31m'
+green = '\033[32m'
+class Neuronio:
+    def __init__(self, conexoes: dict[int, Neuronio], wrapper: Callable[..., Any] | None = None):
         self.conexoes = conexoes
-        self.sucessos = 0
-        self.usos = 0
-    def sucesso(self):
-        self.sucessos += 1
-        self.usos += 1
-    def falha(self):
-        self.usos += 1
+        self.wrapper = wrapper
+    def disparar(self):
+        if self.wrapper:
+            self.wrapper()
+        for peso, neuronio in self.conexoes.items():
+            if peso >= 5:
+                neuronio.disparar()
+    
 
 class RedeNeural:
-    def __init__(self, neurionios: list[Neurônio]):
-        self.neuronios = []
-    def novo_neuronio(self):
+    def __init__(self, layers: tuple[int]):
+        self.layers = layers
+        self.network: list[Neuronio] = []
+    def gen_random_network(self, layer: Optional[int]=None):
+        layer = layer or self.layers[0]
+        match layer:
+            case 0:
+                color = green
+            case len(self.layers):
+                color = red
+            case _:
+                color = ''
+
+        for i in range(layer):
+            if 0 < layer < len(self.layers):
+                self.network[]
